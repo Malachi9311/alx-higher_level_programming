eea94c871b9eae6e95cddf101a1d5eba82bdae74
Malachi Mera <ntuthuko93dlamini@gmail.com>
2023-05-23 14:48:09 +0200

    task04

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// script that counts the number of films Wedge Antilles is in
+const request = require('request');
+const myArgs = process.argv.slice(2);
+let result = 0;
+request(myArgs[0], function (err, response, body) {
+    if (err) {
+        console.log(err);
+    } else {
+        const results = JSON.parse(body).results;
+        let i = 0;
+        for (i = 0; i < results.length; i++) {
+            let j = 0;
+            const character = results[i].characters;
+            for (j = 0; j < character.length; j++) {
+                if (character[j].includes('18')) {
+                    result += 1;
+                }
+            }
+        }
+        console.log(result);
+    }
+});
