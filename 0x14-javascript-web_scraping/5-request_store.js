0081f047e4d000f686a21cb996897ba12e633cf4
Malachi Mera <ntuthuko93dlamini@gmail.com>
2023-05-23 14:49:35 +0200

    task05

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that saves contents of a webpage into a file
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, response, body) {
+    if (error) {
+        console.error(error);
+    }
+    try {
+        fs.writeFile(process.argv[3], body, 'utf8', function (err, result) { if (err) console.log(err); });
+    } catch (err) {
+        console.log(err);
+    }
+});
