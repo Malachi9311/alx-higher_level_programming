1d257ec742dbcb970544c7de1b56de531c7fe91e
Malachi Mera <ntuthuko93dlamini@gmail.com>
2023-05-23 14:43:02 +0200

    task00

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that reads & prints the content of a file
+const fs = require('fs');
+const myArgs = process.argv.slice(2);
+fs.readFile(myArgs[0], 'utf-8', function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
