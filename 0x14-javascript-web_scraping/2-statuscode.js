ff99c7b08a90a84c9f88b7c75aef6f440c0454fc
Malachi Mera <ntuthuko93dlamini@gmail.com>
2023-05-23 14:46:39 +0200

    task02

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that displays the status code of a GET request
+
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
+});
