0a3ac79755fd526565d2a3dbf032c1e1e50d8920
Malachi Mera <ntuthuko93dlamini@gmail.com>
2023-05-23 14:52:35 +0200

    task06

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// script that prints all characters of a Star Wars movie
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url + id, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    }
+    JSON.parse(body).characters.forEach(function (url, callback) {
+        request(url, function (error, response, body) {
+            if (error) {
+                console.error(error);
+            }
+            console.log(JSON.parse(body).name);
+        });
+    });
+});
