bae24b52130abf1027f00fa699614108f873a475
Malachi Mera <ntuthuko93dlamini@gmail.com>
2023-05-23 14:47:22 +0200

    task03

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that displays the title of the Star Wars movie using API
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else if (response.statusCode === 200) {
+        console.log(JSON.parse(body).title);
+    } else {
+        console.log('Error code: ' + response.statusCode);
+    }
+});
